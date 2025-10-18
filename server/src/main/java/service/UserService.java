6f3fdc13103768686be55a291ead842d50c0f1b9
unknown <jarenmclaughlin@gmail.com>
2025-10-18 10:21:22 -0600

    rough userService template

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,22 +1,34 @@
 package service;
 
 import model.*;
+import dataaccess.*;
 
 public class UserService {
+    private final AuthDao authDao = new AuthDao();
+    private final UserDao userDao = new UserDao();
     public AuthData createUser (UserData userData) {
-
+        userDao.createUser(userData);
+        return authDao.createAuthToken(userData.username());
     }
 
     public AuthData login(UserData userData) {
+        userDao.getUserDetails(userData);
+        //verify passwords match
 
+        return authDao.createAuthToken(userData.username());
     }
 
     public void logout(String authToken) {
-
+        authDao.deleteAuthToken(authToken);
     }
 
     public void verifyToken (String authToken) {
-        // Reach out to authDao
+        // Reach out to
+        try {
+            authDao.getAuthToken();
+        } catch () {
+            // not auth error.
+        }
         // if return is null throw no auth error
         // else returns
     }
