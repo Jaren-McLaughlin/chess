3582b735e68a0d98f69a8a456ab0be774422074d
unknown <jarenmclaughlin@gmail.com>
2025-10-25 15:28:57 -0600

    should be authSQLDao

--- server/src/main/java/dataaccess/MySQLDataAccess/AuthSQLDao.java
+++ server/src/main/java/dataaccess/MySQLDataAccess/AuthSQLDao.java
@@ -1,4 +1,86 @@
 package dataaccess.MySQLDataAccess;
 
-public class AuthSQLDao {
+import dataaccess.AuthDao;
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import model.AuthData;
+import model.UserData;
+
+import java.sql.*;
+
+public class AuthSQLDao implements AuthDao {
+    public AuthSQLDao() throws DataAccessException {
+        createTable();
+    }
+
+    public AuthData addUserAuth(String authToken, String username) throws DataAccessException {
+        String statement = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        try (Connection con = DatabaseManager.getConnection()) {
+            PreparedStatement query = con.prepareStatement(statement);
+            query.setString(1, authToken);
+            query.setString(2, username);
+            query.executeUpdate();
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
+        return new AuthData(authToken, username);
+    }
+
+    public String getUserByToken(String authToken) throws DataAccessException {
+        String statement = "SELECT username FROM auth WHERE authToken = ?";
+        try (Connection con = DatabaseManager.getConnection()) {
+            PreparedStatement query = con.prepareStatement(statement);
+            query.setString(1, authToken);
+            ResultSet result = query.executeQuery();
+            if (result.next()) {
+                return result.getString("username");
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
+        return null;
+    }
+
+    public void deleteUserAuth(String authToken) throws DataAccessException {
+        String statement = "DELETE FROM auth WHERE authToken = ?";
+        try (Connection con = DatabaseManager.getConnection()) {
+            PreparedStatement query = con.prepareStatement(statement);
+            query.setString(1, authToken);
+            query.executeUpdate();
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
+    }
+
+    public void clearDb() throws DataAccessException {
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            query.executeUpdate("TRUNCATE TABLE auth");
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
+    }
+
+    private final String[] authTableSql = {
+            """
+        CREATE TABLE IF NOT EXISTS auth (
+          `authToken` varchar(256) NOT NULL,
+          `username` varchar(256) NOT NULL,
+          PRIMARY KEY (`authToken`),
+        )
+        """
+    };
+
+    private void createTable() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection con = DatabaseManager.getConnection()) {
+            for (String statement : authTableSql) {
+                try (var preparedStatement = con.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
+    }
 }
