36f0c39511586ad1dfaa9c63dc6b85529e460eef
unknown <jarenmclaughlin@gmail.com>
2025-10-27 20:27:04 -0600

    gameTests complete

--- server/src/test/java/dataaccess/GameTest.java
+++ server/src/test/java/dataaccess/GameTest.java
@@ -2,9 +2,17 @@ package dataaccess;
 
 import dataaccess.MySQLDataAccess.AuthSQLDao;
 import dataaccess.MySQLDataAccess.GameSQLDao;
+import model.GameData;
+import model.GameListData;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GameTest {
     private GameDao gameDao = null;
@@ -28,38 +36,195 @@ public class GameTest {
 
     @Test
     void addGame() throws DataAccessException {
-
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        String statement = "SELECT * FROM game";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertEquals(1, result.getInt("gameId"));
+                Assertions.assertEquals("test", result.getString("whiteUsername"));
+                Assertions.assertEquals("test2", result.getString("blackUsername"));
+                Assertions.assertEquals("myGame", result.getString("gameName"));
+                Assertions.assertEquals("null", result.getString("game"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
     @Test
-    void someNegativeAddGame() throws DataAccessException {
-
+    void handlesNullInput() throws DataAccessException {
+        gameDao.addGame(new GameData(
+                0,
+                null,
+                null,
+                "CantBeNull",
+                null
+        ));
+        String statement = "SELECT * FROM game";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertEquals(1, result.getInt("gameId"));
+                Assertions.assertNull(result.getString("whiteUsername"));
+                Assertions.assertNull(result.getString("blackUsername"));
+                Assertions.assertEquals("CantBeNull", result.getString("gameName"));
+                Assertions.assertEquals("null", result.getString("game"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
     @Test
     void getGame() throws DataAccessException {
-
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        GameData gameData = gameDao.getGame(1);
+        Assertions.assertEquals(1, gameData.gameID());
+        Assertions.assertEquals("test", gameData.whiteUsername());
+        Assertions.assertEquals("test2", gameData.blackUsername());
+        Assertions.assertEquals("myGame", gameData.gameName());
+        Assertions.assertNull(gameData.game());
     }
     @Test
-    void someNegativeGetGame() throws DataAccessException {
-
+    void getNullGame() throws DataAccessException {
+        GameData gameData = gameDao.getGame(1);
+        Assertions.assertNull(gameData);
     }
     @Test
     void getGameList() throws DataAccessException {
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
 
+        GameListData gameListData = gameDao.getGameList();
+        Assertions.assertEquals(3, gameListData.games().size());
     }
     @Test
-    void someNegativeGetGameList() throws DataAccessException {
-
+    void canGetAnEmptyList() throws DataAccessException {
+        GameListData gameListData = gameDao.getGameList();
+        Assertions.assertEquals(0, gameListData.games().size());
     }
     @Test
     void insertUserIntoGame() throws DataAccessException {
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                null,
+                "myGame",
+                null
+        ));
+        gameDao.insertUserIntoGame(new GameData(
+                1,
+                "test",
+                "UpdatedUsername",
+                "myGame",
+                null
+        ));
 
+        String statement = "SELECT * FROM game";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertEquals(1, result.getInt("gameId"));
+                Assertions.assertEquals("test", result.getString("whiteUsername"));
+                Assertions.assertEquals("UpdatedUsername", result.getString("blackUsername"));
+                Assertions.assertEquals("myGame", result.getString("gameName"));
+                Assertions.assertEquals("null", result.getString("game"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
     @Test
     void someNegativeInsert() throws DataAccessException {
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        DataAccessException thrownError = Assertions.assertThrows(DataAccessException.class, () -> gameDao.insertUserIntoGame(new GameData(
+                1,
+                "test",
+                "test2",
+                null,
+                null
+        )));
 
+        Assertions.assertTrue(thrownError.getMessage().contains("Column 'gameName' cannot be null"));
     }
     @Test
     void clearDb() throws DataAccessException {
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
+        gameDao.addGame(new GameData(
+                0,
+                "test",
+                "test2",
+                "myGame",
+                null
+        ));
 
+        gameDao.clearDb();
+
+        String statement = "SELECT * FROM game";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertNull(result.getString("gameId"));
+                Assertions.assertNull(result.getString("whiteUsername"));
+                Assertions.assertNull(result.getString("blackUsername"));
+                Assertions.assertNull(result.getString("gameName"));
+                Assertions.assertNull(result.getString("game"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
 }
