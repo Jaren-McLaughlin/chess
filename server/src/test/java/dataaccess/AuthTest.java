587d58707f06301fc6870b799dd9efc752d19579
unknown <jarenmclaughlin@gmail.com>
2025-10-27 18:12:43 -0600

    authDao tests completed

--- server/src/test/java/dataaccess/AuthTest.java
+++ server/src/test/java/dataaccess/AuthTest.java
@@ -1,8 +1,15 @@
 package dataaccess;
 
 import dataaccess.MySQLDataAccess.AuthSQLDao;
+import exception.HttpException;
+import model.GameData;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
+import java.sql.*;
 
 public class AuthTest {
     private AuthDao authDao = null;
@@ -13,7 +20,11 @@ public class AuthTest {
             System.out.println("Error starting DB: " + error);
         }
     }
-
+    @BeforeAll
+    static void beforeClear() throws DataAccessException {
+        AuthDao authDao = new AuthSQLDao();
+        authDao.clearDb();
+    }
     @AfterEach
     void clear() throws DataAccessException {
         authDao.clearDb();
@@ -21,30 +32,76 @@ public class AuthTest {
 
     @Test
     void addAuth() throws DataAccessException {
-
+        authDao.addUserAuth("test", "testUser");
+        String statement = "SELECT * FROM auth";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertEquals("test", result.getString("authToken"));
+                Assertions.assertEquals("testUser", result.getString("username"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
     @Test
-    void someNegativeAddAuth() throws DataAccessException {
-
+    void failOnDuplicateUser() throws DataAccessException {
+        authDao.addUserAuth("test", "testUser");
+        DataAccessException thrownError = Assertions.assertThrows(DataAccessException.class, () ->  authDao.addUserAuth("test", "testUser"));
+        Assertions.assertTrue(thrownError.getMessage().contains("Duplicate entry"),
+                "Error message should mention duplicate entry");
     }
     @Test
     void getUserByToken() throws DataAccessException {
-
+        authDao.addUserAuth("test", "testUser");
+        String username = authDao.getUserByToken("test");
+        Assertions.assertEquals("testUser", username);
     }
     @Test
-    void someNegativeGetAuth() throws DataAccessException {
-
+    void returnForNoToken() throws DataAccessException {
+        authDao.addUserAuth("test", "testUser");
+        String username = authDao.getUserByToken("testasdf");
+        Assertions.assertNull(username);
     }
     @Test
     void deleteUserAuth() throws DataAccessException {
-
+        authDao.addUserAuth("test", "testUser");
+        authDao.deleteUserAuth("test");
+        String statement = "SELECT * FROM auth";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertNull(result.getString("authToken"));
+                Assertions.assertNull(result.getString("username"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
     @Test
     void someNegativeDeleteAuth() throws DataAccessException {
-
+        Assertions.assertDoesNotThrow(() -> authDao.deleteUserAuth("test"));
     }
     @Test
     void clearDb() throws DataAccessException {
+        authDao.addUserAuth("test", "testUser");
+        authDao.addUserAuth("test1", "testUser");
+        authDao.addUserAuth("test2", "testUser");
 
+        authDao.clearDb();
+
+        String statement = "SELECT * FROM auth";
+        try (Connection con = DatabaseManager.getConnection()) {
+            Statement query = con.createStatement();
+            ResultSet result = query.executeQuery(statement);
+            if (result.next()) {
+                Assertions.assertNull(result.getString("authToken"));
+                Assertions.assertNull(result.getString("username"));
+            }
+        } catch (DataAccessException | SQLException error) {
+            throw new DataAccessException("SQL Error: " + error);
+        }
     }
 }
